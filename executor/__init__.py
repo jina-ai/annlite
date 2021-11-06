e30e08fca8695260d8451a2aa9f10f3cde57a119
numb3r3 <wangfelix87@gmail.com>
2021-11-06 15:08:59 +0800

    feat: add executor

---
+++ executor/__init__.py
@@ -0,0 +1,64 @@
+from typing import Optional, Iterable
+from jina import Document, DocumentArray, Executor, requests
+from jina.logging.logger import JinaLogger
+import pqlite
+
+
+class PQLiteIndexer(Executor):
+    def __init__(self,
+                 metric: str = 'euclidean',
+                 index_traversal_paths: Iterable[str] = ('r',),
+                 search_traversal_paths: Iterable[str] = ('r',),
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.logger = JinaLogger(self.__class__.__name__)
+
+        self.metric = metric
+        self.index_traversal_paths = index_traversal_paths
+        self.search_traversal_paths = search_traversal_paths
+
+        self._index = pqlite.PQLite(metric=metric, **kwargs)
+
+    @requests(on='/index')
+    def index(self, docs: DocumentArray, parameters: Optional[dict] = {}, **kwargs):
+        traversal_paths = parameters.get('traversal_paths', self.index_traversal_paths)
+        flat_docs = docs.traverse_flat(traversal_paths)
+        if len(flat_docs) == 0:
+            return
+
+        self._index.add(flat_docs)
+
+    @requests(on='/update')
+    def update(self, docs: DocumentArray, parameters: Optional[dict] = {}, **kwargs):
+        traversal_paths = parameters.get('traversal_paths', self.index_traversal_paths)
+        flat_docs = docs.traverse_flat(traversal_paths)
+        if len(flat_docs) == 0:
+            return
+
+        self._index.update(flat_docs)
+
+    @requests(on='search')
+    def search(self, docs: DocumentArray, parameters: Optional[dict] = {}, **kwargs):
+        limit = int(parameters.get('limit', self.limit))
+        traversal_paths = parameters.get('traversal_paths', self.search_traversal_paths)
+        flat_docs = docs.traverse_flat(traversal_paths)
+        if len(flat_docs) == 0:
+            return
+
+        self._index.search(flat_docs, limit=limit)
+
+    @requests(on='/status')
+    def status(self, **kwargs) -> DocumentArray:
+        """Return the document containing status information about the indexer.
+
+        The status will contain information on the total number of indexed and deleted
+        documents, and on the number of (searchable) documents currently in the index.
+        """
+
+        status = Document(tags={})
+        return DocumentArray([status])
+
+    @requests(on='/clear')
+    def clear(self, **kwargs):
+        """Clear the index of all entries."""
+        pass
