cb34c0dc311ae9c7d2efc7aefbcc8dd712600694
David Buchaca Prats <davidbuchaca@gmail.com>
2021-11-22 11:53:56 +0100

    tests: add table numpy comparison vs compiled version

--- tests/test_pq_codec.py
+++ tests/test_pq_codec.py
@@ -25,7 +25,36 @@ def build_pq_codec(build_data):
     pq_codec.fit(Xt)
     return pq_codec
 
+
 def test_pq_adc_table_computation(build_pq_codec, build_data):
+
+    def numpy_adc_table(query, n_subvectors, n_clusters, d_subvector, codebooks):
+        dtable = np.empty((n_subvectors, n_clusters), dtype=np.float32)
+        for m in range(n_subvectors):
+            query_sub = query[m * d_subvector: (m + 1) * d_subvector]
+            dtable[m, :] = np.linalg.norm(codebooks[m] - query_sub, axis=1) ** 2
+
+        return dtable
+
+    pq_codec = build_pq_codec
+    query = build_data[0]
+
+    np_distance_table = numpy_adc_table(query,
+                                        pq_codec.n_subvectors,
+                                        pq_codec.n_clusters,
+                                        pq_codec.d_subvector,
+                                        pq_codec.codebooks)
+
+    distance_table_cy = precompute_adc_table(query,
+                                             pq_codec.d_subvector,
+                                             pq_codec.n_clusters,
+                                             pq_codec.codebooks)
+
+    np_distance_table_cy = np.asarray(distance_table_cy)
+    np.testing.assert_array_almost_equal(np_distance_table, np_distance_table_cy, decimal=5)
+
+
+def test_pq_adc_table_computation_interface(build_pq_codec, build_data):
     pq_codec = build_pq_codec
     query = build_data[0]
 
@@ -35,6 +64,8 @@ def test_pq_adc_table_computation(build_pq_codec, build_data):
                                              pq_codec.d_subvector,
                                              pq_codec.n_clusters,
                                              pq_codec.codebooks)
+
     np_distance_table_cy = np.asarray(distance_table_cy)
 
     np.testing.assert_array_almost_equal(np_distance_table, np_distance_table_cy, decimal=5)
+
