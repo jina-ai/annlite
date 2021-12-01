dc911ac7a5ba2886240b41944be9490531bb0845
numb3r3 <wangfelix87@gmail.com>
2021-12-01 14:42:57 +0800

    feat: add executor

---
+++ executor/executor_pqlite.py
@@ -0,0 +1,135 @@
+from typing import Optional, Iterable
+from jina import Document, DocumentArray, Executor, requests
+from jina.logging.logger import JinaLogger
+import pqlite
+
+
+class PQLiteIndexer(Executor):
+    """
+    A simple Indexer based on PQLite that stores all the Document data together in a local LMDB store.
+
+    To be used as a hybrid indexer, supporting pre-filtering searching.
+    """
+    def __init__(
+        self,
+        dim: int = 0,
+        metric: str = 'euclidean',
+        limit: int = 10,
+        index_traversal_paths: str = 'r',
+        search_traversal_paths: str = 'r',
+        *args,
+        **kwargs
+    ):
+        """
+        :param dim: Dimensionality of vectors to index
+        :param metric: Distance metric type. Can be 'euclidean', 'inner_product', or 'cosine'
+        :param limit: Number of results to get for each query document in search
+        :param index_traversal_paths: Default traversal paths on docs
+        (used for indexing, delete and update), e.g. 'r', 'c', 'r,c'
+        :param search_traversal_paths: Default traversal paths on docs
+        (used for search), e.g. 'r', 'c', 'r,c'
+
+        """
+        super().__init__(*args, **kwargs)
+        self.logger = JinaLogger(self.__class__.__name__)
+
+        self.metric = metric
+        self.limit = limit
+        self.index_traversal_paths = index_traversal_paths
+        self.search_traversal_paths = search_traversal_paths
+
+        self._index = pqlite.PQLite(dim=dim, metric=metric, data_path=self.workspace, **kwargs)
+
+    @requests(on='/index')
+    def index(
+        self, docs: Optional[DocumentArray] = None, parameters: dict = {}, **kwargs
+    ):
+        """Index new documents
+
+        :param docs: the Documents to index
+        :param parameters: dictionary with options for indexing
+        Keys accepted:
+            - 'traversal_paths' (str): traversal path for the docs
+        """
+        if not docs:
+            return
+
+        traversal_paths = parameters.get('traversal_paths', self.index_traversal_paths)
+        flat_docs = docs.traverse_flat(traversal_paths)
+        if len(flat_docs) == 0:
+            return
+
+        self._index.index(flat_docs)
+
+    @requests(on='/update')
+    def update(
+        self, docs: Optional[DocumentArray] = None, parameters: dict = {}, **kwargs
+    ):
+        """Update existing documents
+
+        :param docs: the Documents to update
+        :param parameters: dictionary with options for updating
+        Keys accepted:
+
+            - 'traversal_paths' (str): traversal path for the docs
+        """
+
+        if not docs:
+            return
+
+        traversal_paths = parameters.get('traversal_paths', self.index_traversal_paths)
+        flat_docs = docs.traverse_flat(traversal_paths)
+        if len(flat_docs) == 0:
+            return
+
+        self._index.update(flat_docs)
+
+    @requests(on='/delete')
+    def delete(self, parameters: dict, **kwargs):
+        """Delete entries from the index by id
+
+        :param parameters: parameters to the request
+        """
+        deleted_ids = parameters.get('ids', [])
+
+        self._index.delete(deleted_ids)
+
+    @requests(on='/search')
+    def search(
+        self, docs: Optional[DocumentArray] = None, parameters: dict = {}, **kwargs
+    ):
+        """Perform a vector similarity search and retrieve the full Document match
+
+        :param docs: the Documents to search with
+        :param parameters: dictionary for parameters for the search operation
+        Keys accepted:
+
+            - 'conditions' (dict): the filtering conditions on document tags
+            - 'traversal_paths' (str): traversal paths for the docs
+            - 'limit' (int): nr of matches to get per Document
+        """
+        if not docs:
+            return
+        limit = int(parameters.get('limit', self.limit))
+        traversal_paths = parameters.get('traversal_paths', self.search_traversal_paths)
+        flat_docs = docs.traverse_flat(traversal_paths)
+        if len(flat_docs) == 0:
+            return
+
+        self._index.search(flat_docs, conditions=parameters.get('conditions', None), limit=limit)
+
+    @requests(on='/status')
+    def status(self, **kwargs) -> DocumentArray:
+        """Return the document containing status information about the indexer.
+
+        The status will contain information on the total number of indexed and deleted
+        documents, and on the number of (searchable) documents currently in the index.
+        """
+
+        status = Document(tags=self._index.stat)
+        return DocumentArray([status])
+
+    @requests(on='/clear')
+    def clear(self, **kwargs):
+        """Clear the index of all entries."""
+        self._index.clear()
---
+++ executor/tests/test_executor.py
@@ -0,0 +1,150 @@
+from jina import Document, DocumentArray, Flow, Executor
+
+from executor_pqlite import PQLiteIndexer
+import numpy as np
+import pytest
+
+
+N = 1000  # number of data points
+Nt = 2000
+Nu = 999  # number of data update
+Nq = 10
+D = 128  # dimentionality / number of features
+
+
+def gen_docs(num):
+    res = DocumentArray()
+    k = np.random.random((num, D)).astype(np.float32)
+    for i in range(num):
+        doc = Document(id=i, embedding=k[i])
+        res.append(doc)
+    return res
+
+
+# currently the executor don't have function for training
+# def test_train():
+#     Xt = np.random.random((Nt, D)).astype(
+#         np.float32
+#     )  # 2,000 128-dim vectors for training
+#     pq = PQLite(dim=D, n_cells=64, n_subvectors=8, columns=[('x', float, True)])
+#     pq.fit(Xt)
+#     return pq
+
+
+def test_index(tmpdir):
+    metas = {'workspace': str(tmpdir)}
+    docs = gen_docs(N)
+    f = Flow().add(
+        uses=PQLiteIndexer,
+        uses_with={
+            'dim': D,
+        },
+        uses_metas=metas,
+    )
+    with f:
+        result = f.post(on='/index', inputs=docs, return_results=True)
+        assert sum([len(r.docs) for r in result]) == N
+
+
+def test_update(tmpdir):
+    metas = {'workspace': str(tmpdir)}
+    docs = gen_docs(N)
+    docs_update = gen_docs(Nu)
+    f = Flow().add(
+        uses=PQLiteIndexer,
+        uses_with={
+            'dim': D,
+        },
+        uses_metas=metas,
+    )
+    with f:
+        f.post(on='/index', inputs=docs)
+        update_res = f.post(on='/update', inputs=docs_update, return_results=True)
+        assert sum([len(r.docs) for r in update_res]) == Nu
+
+        res = f.post(on='/status', return_results=True)
+        assert int(res[0].docs[0].tags['total_docs']) == N
+        assert int(res[0].docs[0].tags['index_size']) == N
+
+
+def test_search(tmpdir):
+    metas = {'workspace': str(tmpdir)}
+    docs = gen_docs(N)
+    docs_query = gen_docs(Nq)
+    f = Flow().add(
+        uses=PQLiteIndexer,
+        uses_with={
+            'dim': D,
+        },
+        uses_metas=metas,
+    )
+    with f:
+        f.post(on='/index', inputs=docs)
+        query_res = f.post(on='/search', inputs=docs_query, return_results=True)
+        assert sum([len(r.docs) for r in query_res]) == Nq
+
+        for i in range(len(query_res[0].docs[0].matches) - 1):
+            assert (
+                    query_res[0].docs[0].matches[i].scores['euclidean'].value
+                    <= query_res[0].docs[0].matches[i+1].scores['euclidean'].value
+            )
+
+def test_delete(tmpdir):
+    metas = {'workspace': str(tmpdir)}
+    docs = gen_docs(N)
+    f = Flow().add(
+        uses=PQLiteIndexer,
+        uses_with={
+            'dim': D,
+        },
+        uses_metas=metas,
+    )
+    with f:
+        f.post(on='/index', inputs=docs)
+        res = f.post(on='/status', return_results=True)
+        assert int(res[0].docs[0].tags['total_docs']) == N
+        assert int(res[0].docs[0].tags['index_size']) == N
+
+        f.post(on='/delete', parameters={'ids': ['1', '2', '3', '4', '5']})
+        res = f.post(on='/status', return_results=True)
+        assert int(res[0].docs[0].tags['total_docs']) == N - 5
+        assert int(res[0].docs[0].tags['index_size']) == N - 5
+
+        docs_query = gen_docs(Nq)
+        query_res = f.post(on='/search', inputs=docs_query, return_results=True)
+
+
+
+def test_status(tmpdir):
+    metas = {'workspace': str(tmpdir)}
+    docs = gen_docs(N)
+    f = Flow().add(
+        uses=PQLiteIndexer,
+        uses_with={
+            'dim': D,
+        },
+        uses_metas=metas,
+    )
+    with f:
+        f.post(on='/index', inputs=docs)
+        res = f.post(on='/status', return_results=True)
+        assert int(res[0].docs[0].tags['total_docs']) == N
+        assert int(res[0].docs[0].tags['index_size']) == N
+
+
+def test_clear(tmpdir):
+    metas = {'workspace': str(tmpdir)}
+    docs = gen_docs(N)
+    f = Flow().add(
+        uses=PQLiteIndexer,
+        uses_with={
+            'dim': D,
+        },
+        uses_metas=metas,
+    )
+    with f:
+        f.post(on='/index', inputs=docs)
+        f.post(on='/clear', return_results=True)
+        res = f.post(on='/status', return_results=True)
+        assert int(res[0].docs[0].tags['total_docs']) == 0
+        assert int(res[0].docs[0].tags['index_size']) == 0
