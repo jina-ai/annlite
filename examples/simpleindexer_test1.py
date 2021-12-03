243454062dd521ca67d0f0e38d17ec5f93d1f86a
David Buchaca <davidbuchaca@gmail.com>
2021-12-03 14:19:56 +0100

    fix: add script with weird time

--- examples/simpleindexer_test1.py
+++ examples/simpleindexer_test1.py
@@ -1,21 +1,23 @@
 
 ## Benchmark PQLITE vs SimpleIndexer
 
-from jina import Flow
-import numpy as np
-from jina import Document, DocumentArray
-from jina.math.distance import cdist
-from jina.math.helper import top_k as _top_k
-import pandas as pd
-from sklearn.datasets import make_blobs
-from sklearn.model_selection import train_test_split
 import time
 import os
 import shutil
 
+from jina import Flow
+from jina import Document, DocumentArray
+from jina.math.distance import cdist
+from jina.math.helper import top_k as _top_k
+from jina.logging.profile import TimeContext
+
+import numpy as np
+import pandas as pd
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+
 ## We can download SimpleIndexer and load it  locally or we can use JinaHub
 # from executor_simpleindexer import SimpleIndexer
-
 
 def _precision(predicted, relevant, eval_at):
     """
@@ -25,9 +27,7 @@ def _precision(predicted, relevant, eval_at):
         return 0.0
     predicted_at_k = predicted[:eval_at]
     n_predicted_and_relevant = len(set(predicted_at_k).intersection(set(relevant)))
-
     return n_predicted_and_relevant / len(predicted)
-
 
 def _recall(predicted, relevant, eval_at):
     """
@@ -39,7 +39,6 @@ def _recall(predicted, relevant, eval_at):
     n_predicted_and_relevant = len(set(predicted_at_k).intersection(set(relevant)))
     return n_predicted_and_relevant / len(relevant)
 
-
 def evaluate(predicts, relevants, eval_at):
     recall = 0
     precision = 0
@@ -47,9 +46,7 @@ def evaluate(predicts, relevants, eval_at):
         _predict = np.array([int(x) for x in _predict])
         recall += _recall(_predict, _relevant, top_k)
         precision += _precision(_predict, _relevant, top_k)
-
     return recall / len(predicts), precision / len(predicts)
-
 
 def clean_workspace():
     if os.path.exists('./SimpleIndexer'):
@@ -58,25 +55,22 @@ def clean_workspace():
     if os.path.exists('./workspace'):
         shutil.rmtree('./workspace')
 
-
 Nq = 1
 D = 128
 top_k = 10
 n_cells = 64
 n_subvectors = 64
 n_queries = 1
-n_datasets = [1001, 10_001, 500_001, 1_000_001 ]
-
+n_datasets = [1001, 10_001, 500_001, 1_000_001]
 times = []
 
 for n_examples in n_datasets:
-    time_taken = 0
     clean_workspace()
     np.random.seed(123)
     Xtr, Xte = train_test_split(
         make_blobs(n_samples=n_examples, n_features=D)[0].astype(np.float32), test_size=1
     )
-    print(f'Xtr: {Xtr.shape} vs Xte: {Xte.shape}')
+
     f = Flow().add(
         uses='jinahub://SimpleIndexer',
         uses_with={'match_args': {'metric': 'euclidean',
@@ -90,28 +84,15 @@ for n_examples in n_datasets:
             inputs=docs,
         )
 
-    with f:
+    with TimeContext(f'\n\nn_examples={n_examples}\n\n'):
         t0 = time.time()
+        with f:
+            resp = f.post(
+                on='/search',
+                inputs=DocumentArray([Document(embedding=Xte[0])]),
+                return_results=True,
+            )
+        time_taken = abs(t0 - time.time())
+        print(f'time_taken={time_taken}')
 
-        resp = f.post(
-            on='/search',
-            inputs=DocumentArray([Document(embedding=Xte[0])]),
-            return_results=True,
-        )
-        t1 = time.time()
-        time_taken = time.time() - t0
-        print(f'time_taken = {time_taken}')
-        print(f't1 = {t1}, t0 = {t0}, time_taken={time_taken}')
-
-    print(f'\n\nIndexed {len(docs)} documents')
-    print(f'\nSearch 1 query took {time_taken} sec\n\n')
-    times.append(time_taken)
-    # resp[0].docs DocumentArray with 2 docs
-    # resp[0].docs[0].matches
-    # resp[0].docs[0].matches[0].matches
-
-
-df = pd.DataFrame({'n_examples': n_datasets,'times':times})
-df.to_csv('simpleindexer.csv')
-print(df)
 clean_workspace()
