e316e054dad59f1bfe856db594d3e9da1d657c0c
numb3r3 <wangfelix87@gmail.com>
2021-12-08 02:24:00 +0800

    feat: add filter query language

---
+++ tests/test_filter.py
@@ -0,0 +1,40 @@
+import pytest
+
+from pqlite.filter import Filter
+
+
+def test_empty_filter():
+    f = Filter()
+    where_clause, parameters = f.parse_where_clause()
+    assert where_clause == ''
+    assert parameters == ()
+
+
+def test_simple_filter():
+    f = Filter({'brand': {'$lt': 1}})
+    where_clause, parameters = f.parse_where_clause()
+    assert where_clause == '(brand < ?)'
+    assert parameters == (1,)
+
+
+def test_logic_operator():
+    f = Filter({'$and': {'brand': {'$lt': 1}, 'price': {'$gte': 50}}})
+    where_clause, parameters = f.parse_where_clause()
+    assert where_clause == '(brand < ?) AND (price >= ?)'
+    assert parameters == (1, 50)
+
+    f = Filter({'brand': {'$lt': 1}, 'price': {'$gte': 50}})
+    where_clause, parameters = f.parse_where_clause()
+    assert where_clause == '(brand < ?) AND (price >= ?)'
+    assert parameters == (1, 50)
+
+    f = Filter({'$or': {'brand': {'$lt': 1}, 'price': {'$gte': 50}}})
+    where_clause, parameters = f.parse_where_clause()
+    assert where_clause == '(brand < ?) OR (price >= ?)'
+    assert parameters == (1, 50)
+
+
+def test_error_filter():
+    f = Filter({'$may': {'brand': {'$lt': 1}, 'price': {'$gte': 50}}})
+    with pytest.raises(ValueError):
+        f.parse_where_clause()
