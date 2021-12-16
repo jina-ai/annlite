4652b21a34473c58c8e8805b7cd46ee7de144d20
David Buchaca Prats <davidbuchaca@gmail.com>
2021-12-16 18:59:18 +0100

    fix: do not repeat indexing

--- examples/benchmark_with_filtering.py
+++ examples/benchmark_with_filtering.py
@@ -1,10 +1,11 @@
 
-import numpy as np
 from jina import DocumentArray, Document
 from jina.logging.profile import TimeContext
 from pqlite import PQLite
+
 import os
 import shutil
+import numpy as np
 
 n_index = [10_000, 100_000, 500_000, 1_000_000]
 
@@ -13,12 +14,9 @@ D = 768
 R = 5
 B = 5000
 n_cells = 1
-probs =[[0.10, 0.90/3, 0.90/3, 0.90/3],
-        [0.30, 0.70/3, 0.70/3, 0.70/3],
-        [0.50, 0.50/3, 0.50/3, 0.50/3],
-        [0.80, 0.20/3, 0.20/3, 0.20/3]]
-
-times = {}
+probs =[[0.20, 0.30, 0.50],
+        [0.05, 0.15, 0.80]]
+categories = ['comic', 'movie', 'audiobook']
 
 def clean_workspace():
     if os.path.exists('./data'):
@@ -28,34 +26,33 @@ def clean_workspace():
         shutil.rmtree('./workspace')
 
 
-def docs_with_tags(N, D, probs):
-    categories = ['comic', 'movie', 'audiobook', 'shoes']
-    X = np.random.random((N, D)).astype(np.float32)
-    np.random.seed(123)
-    docs = [
-        Document(
-            id=f'{i}',
-            embedding=X[i],
-            tags={
-                'category': np.random.choice(categories, p=probs),
-            },
-        )
-        for i in range(N)
-    ]
-    da = DocumentArray(docs)
+def docs_with_tags(N, D, probs, categories):
 
-    return da
+    all_docs = []
+    for k,prob in enumerate(probs):
+        n_current = int(N*prob)
+        X = np.random.random((n_current, D)).astype(np.float32)
+
+        docs = [
+            Document(
+                embedding=X[i],
+                tags={
+                    'category': categories[k],
+                },
+            )
+            for i in range(n_current)
+        ]
+        all_docs.extend(docs)
+
+    return DocumentArray(all_docs)
 
 
-results = {}
-
+results = []
 for n_i in n_index:
 
-    results[n_i] = {}
+    results_ni = []
     for current_probs in probs:
-        results[n_i][current_probs[0]] = {}
 
-        times = {}
         clean_workspace()
         columns = [('category', str)]
         idxer = PQLite(
@@ -65,41 +62,40 @@ for n_i in n_index:
             metas={'workspace': './workspace'},
             columns=columns
         )
-        f = {'category': {'$eq': 'comic'}}
 
-        da = docs_with_tags(n_i, D, current_probs)
+        da = docs_with_tags(n_i, D, current_probs, categories)
 
         with TimeContext(f'indexing {n_i} docs') as t_i:
             for i, _batch in enumerate(da.batch(batch_size=B)):
                 idxer.index(_batch)
 
-        times[current_probs[0]] = {}
-        times[current_probs[0]]['index'] = t_i.duration
+        for cat,prob in zip(categories, current_probs):
+            f = {'category': {'$eq': cat}}
 
-        for n_q in n_query:
-            qa = DocumentArray.empty(n_q)
-            q_embs = np.random.random([n_q, D]).astype(np.float32)
-            qa.embeddings = q_embs
-            t_qs = []
+            query_times = []
+            for n_q in n_query:
+                qa = DocumentArray.empty(n_q)
+                q_embs = np.random.random([n_q, D]).astype(np.float32)
+                qa.embeddings = q_embs
+                t_qs = []
 
-            for _ in range(R):
-                with TimeContext(f'searching {n_q} docs') as t_q:
-                    idxer.search(qa, filter=f)
-                t_qs.append(t_q.duration)
-            times[current_probs[0]][f'query_{n_q}'] = np.mean(t_qs[1:])  # remove warm-up
+                for _ in range(R):
+                    with TimeContext(f'searching {n_q} docs') as t_q:
+                        idxer.search(qa, filter=f)
+                    t_qs.append(t_q.duration)
+                query_times.append(np.mean(t_qs[1:]))
 
-        results[n_i][current_probs[0]] = times
+            print(f'\n\nprob={prob}, current_probs={current_probs}, n_i={n_i}\n\n')
+            results_ni.append([n_i, prob, t_i.duration] + query_times)
+
+    results.append(results_ni)
 
 
 title = '| Stored data |% same filter| Indexing time | Query size=1 | Query size=8 | Query size=64|'
 print(title)
 print('|-----' * 6 + '|')
-
-for n_i in n_index:
-    times = results[n_i]
-
-    for current_probs in probs:
-        prob = current_probs[0]
-        for k, v in times[prob].items():
-            s = ' | '.join(f'{v[vv]:.3f}' for vv in ['index', 'query_1', 'query_8', 'query_64'])
-            print(f'| {n_i} | {k} | {s} |')
+for block in results:
+    sorted_elements_in_block = np.argsort([b[1] for b in block])
+    for pos in sorted_elements_in_block:
+        res = block[pos]
+        print(''.join([f'| {x:.3f} ' for x in res] + ['|']))
