08a29b6453db60ec310d05ea6fa7fb46968ca9cc
numb3r3 <wangfelix87@gmail.com>
2021-12-20 12:00:55 +0800

    feat(filter): membership operator

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -34,6 +34,18 @@ def test_logic_operator():
     assert parameters == (1, 50)
 
 
+def test_set_operator():
+    f = Filter({'$and': {'brand': {'$in': ['Nike', 'Gucci']}, 'price': {'$gte': 50}}})
+    where_clause, parameters = f.parse_where_clause()
+    assert where_clause == '(brand IN(?, ?)) AND (price >= ?)'
+    assert parameters == ('Nike', 'Gucci', 50)
+
+    f = Filter({'$or': {'brand': {'$nin': ['Nike', 'Gucci']}, 'price': {'$gte': 50}}})
+    where_clause, parameters = f.parse_where_clause()
+    assert where_clause == '(brand NOT IN(?, ?)) OR (price >= ?)'
+    assert parameters == ('Nike', 'Gucci', 50)
+
+
 def test_error_filter():
     f = Filter({'$may': {'brand': {'$lt': 1}, 'price': {'$gte': 50}}})
     with pytest.raises(ValueError):
