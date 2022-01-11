455695d14cb95e3b64473aacbaef80b3db8b3b1a
numb3r3 <wangfelix87@gmail.com>
2022-01-11 17:44:14 +0800

    fix: add created yaml files

--- examples/k8s_example/create_k8s_yaml.py
+++
@@ -1,8 +0,0 @@
-from jina import Flow
-
-f = Flow(port_expose=8080, protocol='http').add(
-    name='indexer',
-    uses='jinahub+docker://PQLiteIndexer/latest',
-    shards=3,
-)
-f.to_k8s_yaml('./deployment', k8s_namespace='pqlite-namespace')
