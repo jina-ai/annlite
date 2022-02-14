ea04ef8009e042f415561acc4f22a6359720c422
numb3r3 <wangfelix87@gmail.com>
2022-02-15 00:33:09 +0800

    fix: match merger

--- addons/merger.py
+++ addons/merger.py
@@ -1,5 +1,4 @@
-import warnings
-from typing import List, Tuple
+from typing import Optional, Tuple
 
 from jina import DocumentArray, Executor, requests
 
@@ -9,38 +8,27 @@ class MatchMerger(Executor):
     The MatchMerger merges the results of shards by appending all matches..
     """
 
-    def __init__(self, default_traversal_paths: Tuple[str, ...] = '@r', **kwargs):
+    def __init__(
+        self, metric: str = 'cosine', traversal_paths: Tuple[str, ...] = '@r', **kwargs
+    ):
         """
-        :param default_traversal_paths: traverse path on docs, e.g. '@r', '@c'
+        :param metric: Distance metric type. Can be 'euclidean', 'inner_product', or 'cosine'
+        :param traversal_paths: traverse path on docs, e.g. '@r', '@c'
         """
         super().__init__(**kwargs)
-        warnings.warn(
-            'The functionality of MatchMerger is subsumed by the default behaviour starting with'
-            'Jina 3. Consider dropping MatchMerger from your flows. MatchMerger might stop working'
-            'with future versions of Jina or Jina Hub.',
-            DeprecationWarning,
-        )
+        self.metric = metric
+        self.traversal_paths = traversal_paths
 
-        self.default_traversal_paths = default_traversal_paths
-
-    @requests
+    @requests(on='/search')
     def merge(
-        self, docs_matrix: List[DocumentArray] = [], parameters: dict = {}, **kwargs
+        self, docs: Optional[DocumentArray] = None, parameters: dict = {}, **kwargs
     ):
-        traversal_paths = parameters.get(
-            'traversal_paths', self.default_traversal_paths
+        traversal_paths = (
+            parameters.get('traversal_paths', self.traversal_paths) or '@r'
         )
-        results = {}
 
-        if not docs_matrix:
+        if not docs:
             return
-        for docs in docs_matrix:
-            self._merge_shard(results, docs, traversal_paths)
-        return DocumentArray(list(results.values()))
 
-    def _merge_shard(self, results, docs, traversal_paths):
         for doc in docs[traversal_paths]:
-            if doc.id in results:
-                results[doc.id].matches.extend(doc.matches)
-            else:
-                results[doc.id] = doc
+            doc.matches = sorted(doc.matches, key=lambda m: m.scores[self.metric].value)
--- examples/k8s_example/flow.py
+++ examples/k8s_example/flow.py
@@ -8,5 +8,6 @@ f = Flow(port_expose=8080, protocol='grpc').add(
     uses_with={'dim': 512},
     shards=3,
     polling={'/index': 'ANY', '/search': 'ALL', '*': 'ANY'},
+    uses_after='jinahub+docker://MatchMerger/v0.3',
 )
 f.to_k8s_yaml('./deployment')
