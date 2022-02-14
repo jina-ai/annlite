c48e040db31be37149f4e081efd0bd9be73c9774
numb3r3 <wangfelix87@gmail.com>
2022-02-14 11:20:07 +0800

    feat: pool per endpoint

--- examples/k8s_example/flow.py
+++ examples/k8s_example/flow.py
@@ -1,9 +1,12 @@
 from jina import Flow
 
+# POLLING_PER_ENDPOINT:
+# https://github.com/jina-ai/jina/blob/master/tests/unit/orchestrate/flow/flow-orchestrate/test_flow_routing.py#L126
 f = Flow(port_expose=8080, protocol='http').add(
     name='indexer',
     uses='jinahub+docker://PQLiteIndexer/v0.2.3-rc',
     uses_with={'dim': 512},
     shards=3,
+    polling={'/index': 'ANY', '/search': 'ALL', '*': 'ANY'},
 )
 f.to_k8s_yaml('./deployment')
