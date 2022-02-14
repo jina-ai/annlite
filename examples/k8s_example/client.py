5e36efe7a6037fc9b153c0c0e53ede1bcb0d5437
numb3r3 <wangfelix87@gmail.com>
2022-02-14 14:40:58 +0800

    fix: use grpc as default protocol

--- examples/k8s_example/client.py
+++ examples/k8s_example/client.py
@@ -2,42 +2,43 @@ import numpy as np
 from jina import Client, DocumentArray, Flow
 
 #
-#
 # f = Flow(port_expose=4567, protocol='http').add(
 #     name='indexer',
 #     uses='docker://numb3r3/pqlite-executor:latest',
 #     uses_with={'dim': 512},
-#     # shards=3,
+#     shards=3,
+#     polling={'/index': 'ANY', '/search': 'ALL', '*': 'ANY'},
 # )
 #
 # with f:
-#     # client = Client(
-#     #     host='localhost',
-#     #     port='4567',
-#     #     protocol='http',
-#     # )
-#     #
-#     # docs = DocumentArray.empty(5)
-#     # docs.embeddings = np.random.random((5, 512)).astype(np.float32)
-#     #
-#     # result = client.post('/index', inputs=docs, return_results=True)[0]
-#     # print(result)
+#     client = Client(
+#         host='localhost',
+#         port='4567',
+#         protocol='http',
+#     )
+#
+#     docs = DocumentArray.empty(5)
+#     docs.embeddings = np.random.random((5, 512)).astype(np.float32)
+#
+#     result = client.post('/index', inputs=docs, return_results=True)[0]
+#     print(result.docs.summary())
 #     # status = client.post('/status', return_results=True)[0]
 #     # print(status.docs[0].to_dict())
-#     f.block()
+#     # f.block()
 
 
 client = Client(
     host='localhost',
     port='4567',
-    protocol='http',
+    protocol='grpc',
 )
 
-docs = DocumentArray.empty(5)
-docs.embeddings = np.random.random((5, 512)).astype(np.float32)
+N = 5
+docs = DocumentArray.empty(N)
+docs.embeddings = np.random.random((N, 512)).astype(np.float32)
 
-result = client.post('/index', inputs=docs, return_results=True)
-print(result)
+result = client.post('/index', inputs=docs, return_results=True)[0]
+print(result.docs.summary())
 #
 #
 # status = client.post('/status', return_results=True)
