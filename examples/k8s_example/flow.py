8c33bc8543299bd08eec7bd01d4291688a288f05
numb3r3 <wangfelix87@gmail.com>
2022-02-14 11:10:56 +0800

    fix: executor bump tag

--- examples/k8s_example/flow.py
+++ examples/k8s_example/flow.py
@@ -2,7 +2,7 @@ from jina import Flow
 
 f = Flow(port_expose=8080, protocol='http').add(
     name='indexer',
-    uses='jinahub+docker://PQLiteIndexer/latest',
+    uses='jinahub+docker://PQLiteIndexer/v0.2.3-rc',
     uses_with={'dim': 512},
     shards=3,
 )
