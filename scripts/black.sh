bdc1c7a276086b702c807112d0603c245476944a
numb3r3 <wangfelix87@gmail.com>
2022-02-23 15:30:47 +0800

    fix(cicd): add scripts

---
+++ scripts/black.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pip install black==20.8b1
+arrVar=()
+echo we ignore non-*.py files
+excluded_files=(
+   docs/conf.py
+)
+for changed_file in $CHANGED_FILES; do
+  if [[ ${changed_file} == *.py ]] && ! [[ " ${excluded_files[@]} " =~ " ${changed_file} " ]]; then
+    echo checking ${changed_file}
+    arrVar+=(${changed_file})
+  fi
+done
+black -S --check "${arrVar[@]}"
