2ce1ec2283b381f5153ea60141a6bb474bbf0f0c
Gustavo Ye <gusye@mail.ustc.edu.cn>
2022-05-16 22:34:14 +0800

    style(cpp/h): clang-format (#117)
    * chore: format cpp codes
    
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>
    
    * chore(pre-commit): add clang-format check
    
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>
    
    * fix(ignore): ignore vscode and generated cpp
    
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>
    
    * chore: switch clang-format repo
    
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>
    
    * chore(pre-commit): format yaml
    
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>

--- include/hnswlib/hnswlib.h
+++ include/hnswlib/hnswlib.h
@@ -23,87 +23,81 @@
 #endif
 #endif
 
-#include <queue>
-#include <vector>
 #include <iostream>
+#include <queue>
 #include <string.h>
+#include <vector>
 
 namespace hnswlib {
-    typedef size_t labeltype;
+typedef size_t labeltype;
 
-    template <typename T>
-    class pairGreater {
-    public:
-        bool operator()(const T& p1, const T& p2) {
-            return p1.first > p2.first;
-        }
-    };
+template <typename T> class pairGreater {
+public:
+  bool operator()(const T &p1, const T &p2) { return p1.first > p2.first; }
+};
 
-    template<typename T>
-    static void writeBinaryPOD(std::ostream &out, const T &podRef) {
-        out.write((char *) &podRef, sizeof(T));
-    }
-
-    template<typename T>
-    static void readBinaryPOD(std::istream &in, T &podRef) {
-        in.read((char *) &podRef, sizeof(T));
-    }
-
-    template<typename MTYPE>
-    using DISTFUNC = MTYPE(*)(const void *, const void *, const void *);
-
-
-    template<typename MTYPE>
-    class SpaceInterface {
-    public:
-        //virtual void search(void *);
-        virtual size_t get_data_size() = 0;
-
-        virtual DISTFUNC<MTYPE> get_dist_func() = 0;
-
-        virtual void *get_dist_func_param() = 0;
-
-        virtual ~SpaceInterface() {}
-    };
-
-    template<typename dist_t>
-    class AlgorithmInterface {
-    public:
-        virtual void addPoint(const void *datapoint, labeltype label)=0;
-        virtual std::priority_queue<std::pair<dist_t, labeltype >> searchKnn(const void *, size_t) const = 0;
-
-        // Return k nearest neighbor in the order of closer fist
-        virtual std::vector<std::pair<dist_t, labeltype>>
-            searchKnnCloserFirst(const void* query_data, size_t k) const;
-
-        virtual void saveIndex(const std::string &location)=0;
-        virtual ~AlgorithmInterface(){
-        }
-    };
-
-    template<typename dist_t>
-    std::vector<std::pair<dist_t, labeltype>>
-    AlgorithmInterface<dist_t>::searchKnnCloserFirst(const void* query_data, size_t k) const {
-        std::vector<std::pair<dist_t, labeltype>> result;
-
-        // here searchKnn returns the result in the order of further first
-        auto ret = searchKnn(query_data, k);
-        {
-            size_t sz = ret.size();
-            result.resize(sz);
-            while (!ret.empty()) {
-                result[--sz] = ret.top();
-                ret.pop();
-            }
-        }
-
-        return result;
-    }
-
+template <typename T>
+static void writeBinaryPOD(std::ostream &out, const T &podRef) {
+  out.write((char *)&podRef, sizeof(T));
 }
 
-#include "space_l2.h"
-#include "space_ip.h"
+template <typename T> static void readBinaryPOD(std::istream &in, T &podRef) {
+  in.read((char *)&podRef, sizeof(T));
+}
+
+template <typename MTYPE>
+using DISTFUNC = MTYPE (*)(const void *, const void *, const void *);
+
+template <typename MTYPE> class SpaceInterface {
+public:
+  // virtual void search(void *);
+  virtual size_t get_data_size() = 0;
+
+  virtual DISTFUNC<MTYPE> get_dist_func() = 0;
+
+  virtual void *get_dist_func_param() = 0;
+
+  virtual ~SpaceInterface() {}
+};
+
+template <typename dist_t> class AlgorithmInterface {
+public:
+  virtual void addPoint(const void *datapoint, labeltype label) = 0;
+  virtual std::priority_queue<std::pair<dist_t, labeltype>>
+  searchKnn(const void *, size_t) const = 0;
+
+  // Return k nearest neighbor in the order of closer fist
+  virtual std::vector<std::pair<dist_t, labeltype>>
+  searchKnnCloserFirst(const void *query_data, size_t k) const;
+
+  virtual void saveIndex(const std::string &location) = 0;
+  virtual ~AlgorithmInterface() {}
+};
+
+template <typename dist_t>
+std::vector<std::pair<dist_t, labeltype>>
+AlgorithmInterface<dist_t>::searchKnnCloserFirst(const void *query_data,
+                                                 size_t k) const {
+  std::vector<std::pair<dist_t, labeltype>> result;
+
+  // here searchKnn returns the result in the order of further first
+  auto ret = searchKnn(query_data, k);
+  {
+    size_t sz = ret.size();
+    result.resize(sz);
+    while (!ret.empty()) {
+      result[--sz] = ret.top();
+      ret.pop();
+    }
+  }
+
+  return result;
+}
+
+} // namespace hnswlib
+
 #include "bruteforce.h"
 #include "fusefilter.h"
 #include "hnswalg.h"
+#include "space_ip.h"
+#include "space_l2.h"
