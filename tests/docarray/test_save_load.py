610b6156147187e1746bc7f2910fc8dd58eefbc2
jemmyshin <jem.fu@jina.ai>
2022-08-31 16:50:48 +0800

    test: add save load test

---
+++ tests/docarray/test_save_load.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pytest
+from docarray import Document, DocumentArray
+
+
+def test_save_load(tmpdir):
+    save_da = DocumentArray(
+        storage='annlite', config={'n_dim': 768, 'data_path': tmpdir}
+    )
+    for i in range(100):
+        save_da.append(Document(id=str(i), embedding=np.random.rand(768)))
+
+    load_da = DocumentArray(
+        storage='annlite', config={'n_dim': 768, 'data_path': tmpdir}
+    )
+    assert len(load_da) == len(save_da)
+
+    for i in range(100, 120):
+        load_da.append(Document(id=str(i), embedding=np.random.rand(768)))
+    assert len(load_da) == 120
