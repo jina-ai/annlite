ebe119968020b3e9e8bf0bb1df848a933fee5f98
jemmyshin <jem.fu@jina.ai>
2022-08-31 17:03:48 +0800

    refactor: use docs in conttest

--- tests/docarray/test_add.py
+++ tests/docarray/test_add.py
@@ -1,53 +1,32 @@
 import pytest
-from docarray import Document, DocumentArray
+from docarray import DocumentArray
 
 
-def test_add():
+def test_add(docs):
     annlite_doc = DocumentArray(
         storage='annlite',
         config={
-            'n_dim': 3,
+            'n_dim': 4,
         },
     )
 
-    annlite_doc.extend(
-        [
-            Document(id='r0', embedding=[0, 0, 0]),
-            Document(id='r1', embedding=[1, 1, 1]),
-            Document(id='r2', embedding=[2, 2, 2]),
-            Document(id='r3', embedding=[3, 3, 3]),
-            Document(id='r4', embedding=[4, 4, 4]),
-        ]
-    )
+    annlite_doc.extend(docs)
 
     assert len(annlite_doc) == len(annlite_doc[:, 'embedding'])
-    assert len(annlite_doc[:, 'embedding']) == 5
+    assert len(annlite_doc[:, 'embedding']) == 6
 
 
-def test_add_conflict_id():
+def test_add_conflict_id(docs, update_docs):
     annlite_doc = DocumentArray(
         storage='annlite',
         config={
-            'n_dim': 3,
+            'n_dim': 4,
         },
     )
 
-    annlite_doc.extend(
-        [
-            Document(id='r0', embedding=[0, 0, 0]),
-            Document(id='r1', embedding=[1, 1, 1]),
-            Document(id='r2', embedding=[2, 2, 2]),
-            Document(id='r3', embedding=[3, 3, 3]),
-            Document(id='r4', embedding=[4, 4, 4]),
-        ]
-    )
+    annlite_doc.extend(docs)
 
     from sqlite3 import IntegrityError
 
     with pytest.raises(IntegrityError):
-        annlite_doc.extend(
-            [
-                Document(id='r0', embedding=[0, 0, 0]),
-                Document(id='r1', embedding=[1, 1, 1]),
-            ]
-        )
+        annlite_doc.extend(update_docs)
