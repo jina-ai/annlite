ae38015d751e3ff99fb2d75858f0975051c06945
numb3r3 <wangfelix87@gmail.com>
2022-09-23 10:44:00 +0800

    fix(ci): unittest

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -2,7 +2,10 @@
 
 set -ex
 
+BATCH_SIZE=5
+
 declare -a array1=( "tests/test_*.py" )
-dest=( "${array1[@]}" )
+declare -a array2=( "tests/docarray/test_*.py" )
+dest=( "${array1[@]}" "${array2[@]}")
 
 printf '%s\n' "${dest[@]}" | jq -R . | jq -cs .
