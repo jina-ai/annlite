2a7bcc0bcb4f498f1553375efbcd7efa59ca9620
YangXiuyu <gzzyyxy@gmail.com>
2022-10-12 19:35:22 +0800

    fix: test paths

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -4,9 +4,9 @@ set -ex
 
 BATCH_SIZE=5
 
-declare -a array1=( "tests/test_*.py" )
-declare -a array2=( "tests/docarray/test_*.py" )
-declare -a array3=( "tests/executor/test_*.py" )
+declare -a array1=( "annlite/tests/test_*.py" )
+declare -a array2=( "annlite/tests/docarray/test_*.py" )
+declare -a array3=( "annlite/tests/executor/test_*.py" )
 dest=( "${array1[@]}" "${array2[@]}" "${array3[@]}" )
 
 printf '%s\n' "${dest[@]}" | jq -R . | jq -cs .
