4fdacde2a22a5e6fb0edc15bb6f77bf4d8f25834
YangXiuyu <gzzyyxy@gmail.com>
2022-10-17 13:36:51 +0800

    fix: cd workflow (#193)
    * fix: merge build wheels into core-test
    
    * fix: core-test
    
    * fix: build path
    
    * fix: build path
    
    * fix: cd add macos m1
    
    * fix: remove env armv8
    
    * fix: test upload
    
    * fix: test upload .dev11
    
    * fix: add --pre install win
    
    * fix: remote upload test
    
    * fix: remove upload arts ci
    
    * fix: test pre-release
    
    * fix: twine upload testpypi
    
    * fix: add update-version.sh & update cd
    
    * fix: typo

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -11,35 +11,20 @@ INIT_FILE='annlite/__init__.py'
 VER_TAG='__version__ = '
 RELEASENOTE='./node_modules/.bin/git-release-notes'
 
-
-function clean_build {
-    rm -rf dist
-    rm -rf annlite/*.so
-    rm -rf *.egg-info
-    rm -rf build
+function escape_slashes {
+    sed 's/\//\\\//g'
 }
 
+function update_ver_line {
+    local OLD_LINE_PATTERN=$1
+    local NEW_LINE=$2
+    local FILE=$3
 
-function pub_pypi {
-    # publish to pypi
-    twine upload --repository testpypi dist/*
-    clean_build
+    local NEW=$(echo "${NEW_LINE}" | escape_slashes)
+    sed -i '/'"${OLD_LINE_PATTERN}"'/s/.*/'"${NEW}"'/' "${FILE}"
+    head -n10 ${FILE}
 }
 
-function git_commit {
-    git config --local user.email "dev-bot@jina.ai"
-    git config --local user.name "Jina Dev Bot"
-    git tag "v$RELEASE_VER" -m "$(cat ./CHANGELOG.tmp)"
-    git add $INIT_FILE ./CHANGELOG.md
-    git commit -m "chore(version): the next version will be $NEXT_VER" -m "build($RELEASE_ACTOR): $RELEASE_REASON"
-}
-
-
-function make_release_note {
-    ${RELEASENOTE} ${LAST_VER}..HEAD .github/release-template.ejs > ./CHANGELOG.tmp
-    head -n10 ./CHANGELOG.tmp
-    printf '\n%s\n\n%s\n%s\n\n%s\n\n%s\n\n' "$(cat ./CHANGELOG.md)" "<a name="release-note-${RELEASE_VER//\./-}"></a>" "## Release Note (\`${RELEASE_VER}\`)" "> Release time: $(date +'%Y-%m-%d %H:%M:%S')" "$(cat ./CHANGELOG.tmp)" > ./CHANGELOG.md
-}
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
@@ -67,12 +52,9 @@ if [[ $1 == "final" ]]; then
   NEXT_VER=$(echo $RELEASE_VER | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
   printf "bump master version to: \e[1;32m$NEXT_VER\e[0m\n"
 
-  make_release_note
-  pub_pypi
+  VER_TAG_NEXT=$VER_TAG\'${NEXT_VER}\'
+  update_ver_line "$VER_TAG" "$VER_TAG_NEXT" "$INIT_FILE"
 
-  RELEASE_REASON="$2"
-  RELEASE_ACTOR="$3"
-  git_commit
 elif [[ $1 == 'rc' ]]; then
   printf "this will be a release candidate: \e[1;33m$RELEASE_VER\e[0m\n"
   DOT_RELEASE_VER=$(echo $RELEASE_VER | sed "s/rc/\./")
@@ -80,18 +62,16 @@ elif [[ $1 == 'rc' ]]; then
   NEXT_VER=$(echo $NEXT_VER | sed "s/\.\([^.]*\)$/rc\1/")
   printf "bump master version to: \e[1;32m$NEXT_VER\e[0m, this will be the next version\n"
 
-  make_release_note
-  pub_pypi
-
-  RELEASE_REASON="$2"
-  RELEASE_ACTOR="$3"
-  git_commit
+  VER_TAG_NEXT=$VER_TAG\'${NEXT_VER}\'
+  update_ver_line "$VER_TAG" "$VER_TAG_NEXT" "$INIT_FILE"
+  
 else
   # as a prerelease, pypi update only, no back commit etc.
   COMMITS_SINCE_LAST_VER=$(git rev-list $LAST_VER..HEAD --count)
   NEXT_VER=$RELEASE_VER".dev"$COMMITS_SINCE_LAST_VER
   printf "this will be a developmental release: \e[1;33m$NEXT_VER\e[0m\n"
 
-  pub_pypi
+  VER_TAG_NEXT=$VER_TAG\'${NEXT_VER}\'
+  update_ver_line "$VER_TAG" "$VER_TAG_NEXT" "$INIT_FILE"
 
 fi
